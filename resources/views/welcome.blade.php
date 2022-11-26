873c1948cc8b378b93c4a00928ed9236a2d6a3bd
hamzah <hamzah.madni01@gmail.com>
2022-11-26 11:13:00 +0000

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="antialiased">
+        <div class="max-w-7xl mx-auto">
+            <div class="flex justify-between m-2 p-2">
+                <form action=""></form>
+                <a href=""></a>
+            </div>
+            
+        </div>
+    </body>
+</html>
